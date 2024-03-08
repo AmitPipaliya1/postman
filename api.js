39daa8d1654c1e76623b7ec9d9594cc3b4373f94
DevidOfs <devidparmar2907@gmail.com>
2024-03-08 16:23:22 +0530

    data are added

---
+++ api.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+$(document).ready(function (){
+
+
+    $("#form").click(function(){
+
+      // console.log($("#post").val());
+        $.ajax({
+            type: $("#option").val(),
+            dataType:"json",
+            url: $("#post").val(),
+             data: {
+               email:$('#exampleInputEmail1').val(),
+               password : $('#exampleInputPassword1').val()
+            },
+            success: function (data, status, xhr) {
+              console.log(data);
+              $("#response").append(`<P>${JSON.stringify(data,null,2)}</p>`)
+            }
+          });
+    })
+}) ;
